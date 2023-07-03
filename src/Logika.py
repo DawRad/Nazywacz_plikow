7425f14c1364c5cbc89a13ee4d5593e00852d59c
DawRad <dawidissimo@gmail.com>
2023-07-04 01:56:21 +0200

    Modyfikacja metody klasy "Zarzadca" zmieniającej nazwę pliku - możliwość zmiany rozszerzenia pliku

--- src/Logika.py
+++ src/Logika.py
@@ -24,7 +24,22 @@ class Zarzadca:
         res = os.listdir(sciezka)
         return res
     
-    def zmienNazwePliku(self, stara_nazwa: str, nowa_nazwa: str, sciezka:str = ''):
+    def zmienNazwePliku(self, stara_nazwa: str, nowa_nazwa: str, sciezka:str = '', rozszerzenie: str = ''):
+        spr_rozszerz = nowa_nazwa.split('.')
+
         stara_nazwa = sciezka + stara_nazwa
-        nowa_nazwa = sciezka + nowa_nazwa
+        if rozszerzenie == '' and len(spr_rozszerz) == 1:
+            stare_rozszerz = stara_nazwa.split('.')
+
+            if len(stare_rozszerz) == 1:
+                #TODO:
+                #   trzeba jakoś zareagować, gdyby stara_nazwa też nie miała rozszerzenia
+                pass
+            
+            nowa_nazwa = sciezka + nowa_nazwa + '.' + stare_rozszerz[len(stare_rozszerz) - 1]
+        elif rozszerzenie != '' and len(spr_rozszerz) == 1:
+            nowa_nazwa = sciezka + nowa_nazwa + '.' + rozszerzenie
+        elif len(spr_rozszerz) > 1:
+            nowa_nazwa = sciezka + nowa_nazwa
+
         os.rename(stara_nazwa, nowa_nazwa)      
