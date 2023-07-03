c0bc9fec133f2f96314167a775e5f4b605c92806
DawRad <dawidissimo@gmail.com>
2023-07-04 01:15:39 +0200

    Dodanie metody klasy Zarzadca zmieniającej nazwę pliku

--- src/Logika.py
+++ src/Logika.py
@@ -20,6 +20,11 @@ class Zarzadca:
         ----------
         list() : Listę nazw plików.
         """
-        
+
         res = os.listdir(sciezka)
         return res
+    
+    def zmienNazwePliku(self, stara_nazwa: str, nowa_nazwa: str, sciezka:str = ''):
+        stara_nazwa = sciezka + stara_nazwa
+        nowa_nazwa = sciezka + nowa_nazwa
+        os.rename(stara_nazwa, nowa_nazwa)      
